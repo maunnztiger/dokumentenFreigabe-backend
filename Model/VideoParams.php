02ab98a5bc1ad85504aa21add6a73295fccd028e
igorwestphal2 <igorwestphal@yahoo.de>
2021-03-03 09:12:18 +0100

    adding third party library

---
+++ Model/VideoParams.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace contentFreigabe\Model;
+use contentFreigabe\ffmpeg;
+use contentFreigabe\Model\Model;
+
+class VideoParams
+{
+
+    private $videoNames;
+    private $duration;
+    private $increasedPlays;
+    private $plays;
+    private $screenshot;
+
+    public function getVideoNames()
+    {
+        $files = scandir("C:/xampp/htdocs/assets");
+
+        foreach ($files as $key => $file) {
+            if ($files[$key] !== "." && $files[$key] !== ".." && strpos($files[$key], '.jpg') === false) {
+                $this->videoNames[] = trim($files[$key], '.mp4');
+            }
+        }
+
+        return $this->videoNames;
+    }
+     /**
+     * 
+     */
+    public function getVideoDurationTime($filepath){
+       
+        $dur = shell_exec("ffmpeg -i ".$filepath.".mp4"." 2>&1");
+      
+        if(preg_match("/: Invalid /", $dur)){
+            return false;
+         }
+         preg_match("/Duration: (.{2}):(.{2}):(.{2})/", $dur, $this->duration);
+         if(!isset($this->duration[1])){
+            return false;
+         }
+         return $this->duration[1].":".$this->duration[2].":".$this->duration[3];
+     }
+
+     
+     public function getPlays($videoName){
+        $model = new Model();
+        $this->plays = $model->select('plays')->from('video')
+        ->where('video_name', ':name')
+        ->executeQuery(':name', $videoName)->as_object()->plays;
+     
+        return $this->plays;
+     }
+
+     public function setPlays($videoName){
+        $numberOfPlays = $this->getPlays($videoName);
+        if(is_int($numberOfPlays)){
+            $this->increasedPlays = $numberOfPlays+1;
+            var_dump($this->increasedPlays);
+        } else {
+            return false;
+        }
+     }
+
+     public function savePlays($videoName){
+        $this->setPlays($videoName);
+            $model = new Model();
+            $model->update('video')->set(array(
+                'plays'
+            ),
+            array(
+                ':plays')
+            )->where('video_name', ':name')
+            ->executeQuery(
+                array(
+                ':plays',
+                ':name'
+                ),
+                array(
+                    $this->increasedPlays,
+                    $videoName
+                )
+            );
+          
+      
+        
+        
+     }
+
+     
+
+     public function getScreenshot($videpath, $name){
+        $time = 10;
+        $infile = "C:\\xampp\\htdocs\\assets\\Detroit.mp4";
+        $thumbnail = "C:\\xampp\\htdocs\\assetsDetroit.jpg";
+        
+        $cmd = sprintf(
+            "ffmpeg -i %s -an -ss %s.001 -y -f mjpeg %s 2>&1",
+               
+               $infile, $time, $thumbnail
+        ); 
+        
+        $img = exec($cmd);
+        var_dump($img);
+        
+     }
+
+}
